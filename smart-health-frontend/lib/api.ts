d41f181bb2824f0cc721edba51da8d16314c445a
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-06-27 22:51:12 +0530

    Update api.ts

--- smart-health-frontend/lib/api.ts
+++ smart-health-frontend/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://13.201.82.56:3001' || 'http://localhost:3001', 
+    baseURL: process.env.NEXT_PUBLIC_API_URL, //'http://localhost:3001', 
     withCredentials: true,
 });
 
