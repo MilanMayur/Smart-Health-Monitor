b9912018fe63e539b5b45cbc6a4ac7d9bca7f23b
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-06-27 23:00:41 +0530

    Update next.config.js

--- smart-health-frontend/next.config.js
+++ smart-health-frontend/next.config.js
@@ -1,14 +1,14 @@
 //next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  /*async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: 'http://localhost:5000/:path*', // <- your NestJS backend
       },
     ];
-  },
+  },*/
 };
 
 module.exports = nextConfig;
