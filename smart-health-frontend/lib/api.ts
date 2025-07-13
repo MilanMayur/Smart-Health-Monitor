deeccdfc3db824a1e80b419ea256c5565f601ec8
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-07-13 16:02:19 +0530

    Delete smart-health-frontend directory

--- smart-health-frontend/lib/api.ts
+++
@@ -1,9 +0,0 @@
-// lib/api.ts
-import axios from 'axios';
-
-const API = axios.create({
-    baseURL: 'http://<ec2-ip>:3001', //process.env.NEXT_PUBLIC_API_URL, http://localhost:3001 
-    withCredentials: true,
-});
-
-export default API;
