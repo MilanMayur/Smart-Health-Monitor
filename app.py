d900846ed125757672dbde00717a420249a66d08
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-08-09 00:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 # app.py
 import tkinter as tk
 from utils.db import client
+import subprocess
+import time
+import atexit
+import os
+import sys
+
+# Start Flask backend
+try:
+    flask_process = subprocess.Popen(
+        [sys.executable, os.path.join("ml-services", "app.py")],
+        stdout=subprocess.PIPE, # Disables flask log
+        stderr=subprocess.PIPE # Disables flask log
+    )
+except Exception as e:
+    print("Failed to start Flask backend:", e)
+    sys.exit(1)
+
+# Make sure Flask has started
+time.sleep(2)
+
+# Kill Flask on exit
+def cleanup():
+    flask_process.terminate()
+atexit.register(cleanup)
 
 # DB Check
 try:
@@ -14,6 +38,8 @@ def main():
     from views.intro import show_intro_page
     from session import load_session
     from views.dashboard import open_dashboard
+
+    print("Smart Health Monitor started successfully.")
 
     root = tk.Tk()
     root.title("Smart Health Monitor")
@@ -30,4 +56,10 @@ def main():
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print("Fatal error in Smart Health Monitor:", e)
+        cleanup()  # Ensure Flask is killed
+        sys.exit(1)
+
